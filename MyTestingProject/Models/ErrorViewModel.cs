187759c918fdeec7e769d723f779676dcca95283
Mohamad Abdou <Mohamad-M-Abdou@hotmail.com>
2024-08-03 16:26:00 +0300

    Add project files.

---
+++ MyTestingProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyTestingProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
